087926f909798f4bf73e9251baa77605c8e3da10
lihaoyun6 <lihaoyun11@gmail.com>
2021-08-19 01:14:32 +0800

    Update nsci.rb

--- nsci.rb
+++ nsci.rb
@@ -4,9 +4,9 @@
 class Nsci < Formula
 	desc "Night Shift Command-line Interface"
 	homepage ""
-	url "https://cdn.jsdelivr.net/gh/lihaoyun6/web/homebrew-pack/nsci-0.3.8.tar.gz"
-	version "0.3.8"
-	sha256 "6dd7400406df26338c2ca1f86cda47b69edb9fcf968bb9459579cb9c8579a04d"
+	url "https://cdn.jsdelivr.net/gh/lihaoyun6/web/homebrew-pack/nsci-0.3.9.tar.gz"
+	version "0.3.9"
+	sha256 "ae2483cd0c96e0435b28fae92dae03a4d90a49cca29d5fe6b9dbd92a4b079924"
 	depends_on "lihaoyun6/tap/getlocal"
 	# depends_on "cmake" => :build
 
