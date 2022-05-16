6057951388f337a3a0d610c106ac6d9aa1dcf3bc
lihaoyun6 <lihaoyun11@gmail.com>
2022-05-16 16:34:02 +0800

    Update appdimmer.rb

--- Casks/appdimmer.rb
+++ Casks/appdimmer.rb
@@ -1,6 +1,6 @@
 cask "appdimmer" do
-  version "0.3.4"
-  sha256 "5682f404847b215cef440a4c0b065ec906011410a383d2c97e75674ed225b64d"
+  version "0.3.5"
+  sha256 "0daa5de00649b14840484bcee2919d159302c308c3a8ed2355bf2a5249f3f4ef"
 
   url "https://github.com/lihaoyun6/AppDimmer/releases/download/#{version}/AppDimmer_v#{version}.dmg"
   name "AppDimmer"
