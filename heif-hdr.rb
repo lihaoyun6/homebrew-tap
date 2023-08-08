b595166602a2e59387c93ebcedbf46b50d11a1a8
lihaoyun6 <lihaoyun11@gmail.com>
2023-08-08 17:04:41 +0800

    Update heif-hdr.rb

--- heif-hdr.rb
+++ heif-hdr.rb
@@ -2,10 +2,10 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class HeifHdr < Formula
-  version "0.0.1"
+  version "0.0.3"
 	desc "Tool to split or create Apple HDR HEIF/HEIC photos"
 	url "https://cdn.jsdelivr.net/gh/lihaoyun6/web/homebrew-pack/heif-hdr_v#{version}.zip"
-	sha256 "9cd078cf791938aaa7576f7d224cfbd255489ccc1ed87b796fc385bc30f7d43a"
+	sha256 "9ba060eff2937e53c7ba0c0ffe23566aba888b89c2444566dc20e00d48338834"
 	# depends_on "cmake" => :build
 
 	def install
