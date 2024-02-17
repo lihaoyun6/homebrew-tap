b864dab08f7d9c6ee5cbe0d245d2ff484cce1f38
lihaoyun6 <lihaoyun11@gmail.com>
2024-02-17 20:04:34 +0800

    Update dockbattery.rb

--- Casks/dockbattery.rb
+++ Casks/dockbattery.rb
@@ -1,6 +1,6 @@
 cask "dockbattery" do
-  version "1.1.3"
-  sha256 "7178d81540a9bd1176408b57912851ae9d9c15638aef36ee62f09525e7f0151f"
+  version "1.1.4"
+  sha256 "cb716da5145bba96e5cba4d024f8ae6ec9c508b6d726e9abc7fa74b545f02570"
 
   url "https://github.com/lihaoyun6/DockBattery/releases/download/#{version}/DockBattery_v#{version}.dmg"
   name "DockBattery"
