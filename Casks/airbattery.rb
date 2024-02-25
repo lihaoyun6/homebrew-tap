36b477af5f673dc4133f1409a53d3d1929b57816
lihaoyun6 <lihaoyun11@gmail.com>
2024-02-26 01:12:45 +0800

    Create airbattery.rb

---
+++ Casks/airbattery.rb
@@ -0,0 +1,11 @@
+cask "airbattery" do
+  version "1.2.0"
+  sha256 "22ec31472bf362c738703139b9c9440ba4f92dba58ed1c2742989997b5f8dfcf"
+
+  url "https://github.com/lihaoyun6/AirBattery/releases/download/#{version}/AirBattery_v#{version}.dmg"
+  name "AirBattery"
+  desc "Get the battery level of all your devices on your Mac."
+  homepage "https://github.com/lihaoyun6/AirBattery"
+
+  app "AirBattery.app"
+end
