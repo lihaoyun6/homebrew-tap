186fb6c36e66d6067ff4e7d48767c25bbfb5d7e6
lihaoyun6 <lihaoyun11@gmail.com>
2024-05-02 01:07:04 +0800

    Update quickrecorder.rb

--- Casks/quickrecorder.rb
+++ Casks/quickrecorder.rb
@@ -1,6 +1,6 @@
 cask "quickrecorder" do
-  version "1.1.2"
-  sha256 "0533fde17b2a3fd099c77de8f461e8858164d0ec030efa129de051995aa99626"
+  version "1.1.3"
+  sha256 "0ad1c1fe375b9874c3c0373ef09e176c88681ba6efe264fcb05e9edb60cb79a2"
 
   url "https://github.com/lihaoyun6/QuickRecorder/releases/download/#{version}/QuickRecorder_v#{version}.dmg"
   name "QuickRecorder"
