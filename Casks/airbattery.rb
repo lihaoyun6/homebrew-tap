f103813b843a43a0a26e21d8569d059879df347e
lihaoyun6 <lihaoyun11@gmail.com>
2024-06-23 23:42:45 +0800

    Update airbattery.rb

--- Casks/airbattery.rb
+++ Casks/airbattery.rb
@@ -1,6 +1,6 @@
 cask "airbattery" do
-  version "1.3.7"
-  sha256 "17b0889169dc6cefd95ec13959a0dd8bdd037731e38652da1ee8e76d1a77d36e"
+  version "1.4.0"
+  sha256 "aa144f37a7be0131b36bf2a1102cb69804f4139fbce51d543be171deb558d12f"
 
   url "https://github.com/lihaoyun6/AirBattery/releases/download/#{version}/AirBattery_v#{version}.dmg"
   name "AirBattery"
