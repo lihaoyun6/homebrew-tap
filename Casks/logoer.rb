796e8a09cd00e3359da0913631fde78335109b17
lihaoyun6 <lihaoyun11@gmail.com>
2024-07-27 00:46:04 +0800

    Update logoer.rb

--- Casks/logoer.rb
+++ Casks/logoer.rb
@@ -1,6 +1,6 @@
 cask "logoer" do
-  version "0.1.8"
-  sha256 "0409800c06c35e01fd44b9685acbddb84c0f973128e64962d96da7bf1a13ab74"
+  version "0.1.9"
+  sha256 "bb44c559b0c8ce868f91aef6e5ddfb0819a5219a4e84e985c7ec5519e050cf91"
 
   url "https://github.com/lihaoyun6/Logoer/releases/download/#{version}/Logoer_v#{version}.dmg"
   name "Logoer"
