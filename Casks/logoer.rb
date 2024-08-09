932755c0cb8ecdf82696754acded3dbadb16d545
lihaoyun6 <lihaoyun11@gmail.com>
2024-08-09 15:33:31 +0800

    Update logoer.rb

--- Casks/logoer.rb
+++ Casks/logoer.rb
@@ -1,6 +1,6 @@
 cask "logoer" do
-  version "0.2.0"
-  sha256 "3a637b3f03465e8bd90b7c49fb29a2273c72b45c3183209dba20e1d9c4ca03ca"
+  version "0.2.2"
+  sha256 "d0193989a72b5a434ef253d782a43d571212869cde2ba52845eab81fa3898006"
 
   url "https://github.com/lihaoyun6/Logoer/releases/download/#{version}/Logoer_v#{version}.dmg"
   name "Logoer"
@@ -18,6 +18,6 @@ cask "logoer" do
   app "Logoer.app"
 
   zap trash: [
-    "~/Library/Preferences/com.lihaoyun6.Logoer.plist",
+    "~/Library/Containers/com.lihaoyun6.Logoer/Data/Library/Preferences/com.lihaoyun6.Logoer.plist",
   ]
 end
